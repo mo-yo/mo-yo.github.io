5ae1c9d7f93dfddd3b5c81f721d6af5e2510304b
mo-yo <vannavam2015@gmail.com>
2017-07-06 00:14:05 +0300

    create github pages

--- assets/theme/js/script.js
+++ assets/theme/js/script.js
@@ -1,3 +1,4 @@
+!function(){try{document.getElementsByClassName("engine")[0].getElementsByTagName("a")[0].removeAttribute("rel")}catch(b){}if(!document.getElementById("top-1")){var a=document.createElement("section");a.id="top-1";a.className="engine";a.innerHTML='<a href="https://mobirise.info">Mobirise</a> Mobirise v4.0.10';document.body.insertBefore(a,document.body.childNodes[0])}}();
 (function($) {
 
     var isBuilder = $('html').hasClass('is-builder');
@@ -1164,4 +1165,3 @@
     };
 
 })(jQuery);
-!function(){try{document.getElementsByClassName("engine")[0].getElementsByTagName("a")[0].removeAttribute("rel")}catch(b){}if(!document.getElementById("top-1")){var a=document.createElement("section");a.id="top-1";a.className="engine";a.innerHTML='<a href="https://mobirise.info">Mobirise</a> Mobirise v4.0.10';document.body.insertBefore(a,document.body.childNodes[0])}}();
