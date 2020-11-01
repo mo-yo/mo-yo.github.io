b0b245afb3c097f49bd0213810c0d2003b0529da
vannalux <remtexvanna@gmail.com>
2020-11-01 22:16:26 +0200

    5

--- imsearch.php
+++ imsearch.php
@@ -19,7 +19,7 @@
 		
 		
 		<link rel="stylesheet" href="pluginAppObj/imSideBar_pluginAppObj_02/custom.css" media="screen, print" />
-		<link rel="stylesheet" href="pcss/imsearch.css?2020-3-4-0-637398627723391822" media="screen,print" />
+		<link rel="stylesheet" href="pcss/imsearch.css?2020-3-4-0-637398649429332740" media="screen,print" />
 		<script src="res/jquery.js?2020-3-4-0"></script>
 		<script src="res/x5engine.js?2020-3-4-0" data-files-version="2020-3-4-0"></script>
 		
@@ -30,7 +30,7 @@
 			x5engine.utils.currentPagePath = 'imsearch.php';
 			x5engine.boot.push(function () { x5engine.imPageToTop.initializeButton({}); });
 		</script>
-		<link rel="icon" href="favicon.png?2020-3-4-0-637398627723311764" type="image/png" />
+		<link rel="icon" href="favicon.png?2020-3-4-0-637398649429176669" type="image/png" />
 	</head>
 	<body>
 		<div id="imPageExtContainer">
